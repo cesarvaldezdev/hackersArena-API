6678a6dda30c65a9a9427199b55b3db52cb5b421
francoarielramirezvilla <franco.ramirez@alumno.udg.mx>
2018-10-01 15:13:08 -0500

    Modificaciones importantes a peticion selectAll de bd

--- db/index.js
+++ db/index.js
@@ -7,52 +7,68 @@ class DB{
   constructor(){
     this.knex=connect();
   }
-  selectAll(table){
+  selectAll(table,columns,filters,order,asc,limit){
+    let filts='';
+    let ord='asc';
+    if (filters!='') {
+      filters.forEach((i, index) => {
+        if (index !== 0) filts += `${i.logic} `;
+        filts += `${i.attr.replace('`', '').replace('`', '')} ${i.oper} `;
+        if (i.oper === 'LIKE') extra += `'%${i.val.replace('\'', '').replace('\'', '')}%' `;
+        else filts += `${i.val} `;
+      });
+    }
+    if(columns=='') columns='*';
+    if(!asc) ord='desc';
     return this.knex
-      .select('*')
+      .select(columns)
       .from(table)
+      .whereRaw(filts)
+      .orderBy(order,ord)
+      .limit(limit)
       .then((results) => {
         return results;
-      }).catch(function(error) {
-        console.error(error);
-      });
+      }).catch(function(e) { console.error(e); });
   }
 
-  selectOne(table,id){
+  selectOne(table,columns,filter){
     return this.knex
       .select('*')
       .from(table)
       .where('id',id)
       .then((results)=>{
         return results;
-      }).catch(function(error) {
-        console.error(error);
-      });
+      }).catch(function(e) { console.error(e); });
   }
 }
 
 function connect () {
-  // [START connect]
   const config = {
     user: process.env.SQL_USER,
     password: process.env.SQL_PASSWORD,
     database: process.env.SQL_DATABASE
   };
-
   if (process.env.INSTANCE_CONNECTION_NAME && process.env.NODE_ENV === 'production') {
     config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
   }
   config.socketPath = `/cloudsql/${process.env.INSTANCE_CONNECTION_NAME}`;
-
-  // Connect to the database
   const knex = Knex({
     client: 'mysql',
     connection: config
   });
-  // [END connect]
-
   return knex;
 }
 
-
 module.exports = new DB();
+
+
+// selectAll(table){
+//   return this.knex
+//     .select('*')
+//     .from(table)
+//     .then((results) => {
+//       return results;
+//     }).catch(function(error) {
+//       console.error(error);
+//     });
+// }
