20f7448013b4f03413528473ef2b40121433a239
Dany Hdez <42160021+naekkeoya@users.noreply.github.com>
2018-10-02 09:26:23 -0500

    Update verdict.js

--- models/verdict.js
+++ models/verdict.js
@@ -9,24 +9,39 @@ class Verdict {
     this.id = id;
     this.type = type;
   }
-
+  
+  //Regresa todos los elementos que cumplen con las restricciones establecidas
   static async getAll() {
     const data = processResult(await db.selectAll('Verdict','','','id',true,10));
     return data;
   }
 
+  //Regresa un solo elemento que cumple con las restricciones establecidas
   static async get(verdictId) {
     const data = await db.selectOne('Verdict', '',[{attr:'id',oper:'=',val:verdictId}]);
     return data.length !== 0 ? new Verdict(data[0]) : data;
   }
 
-  static async create({ type }) {
-    let response = await db.insert('Verdict', { type });
-    const id = response.insertId;
-    if (id > 0) {
-      return new Verdict({ id, type });
+  // Actualiza el elemento en la tabla si este ya existe, sino lo crea
+  static async save(){
+    if (this.id !== undefined && this.processResult(await db.get('Verdict', 'id', [{ attr: 'id', oper: '=', val: this.id }])).length !== 0) return this.update();
+    if (await db.create('Verdicts', this)) return 0;
+    return 1;
+  }
+  
+  // Actualiza el elemento
+  static async update() {
+    if (this.id !== undefined && await db.update('Verdict', this, [{ attr: 'id', oper: '=', val: this.id }])) return 0;
+    return 1;
+  }
+  
+  // Elimina el elemento en la tabla por indice
+  static async delete() {
+    if (this.id !== undefined && processResult(await db.get('Verdict', 'id', [{ attr: 'id', oper: '=', val: this.id }])).length !== 0) {
+      if (this.id !== undefined && await db.delete('Verdict', [{ attr: 'id', oper: '=', val: this.id }]) !== undefined) return 0;
+      return 1;
     }
-    return [];
+    return 2;
   }
 }
 
