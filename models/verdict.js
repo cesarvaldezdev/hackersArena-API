7fdbd19b1e075c4db886179ac722c206a1714416
francoarielramirezvilla <franco.ramirez@alumno.udg.mx>
2018-10-01 23:36:03 -0500

    Plantilla actualizada para Modelos

--- models/verdict.js
+++ models/verdict.js
@@ -5,31 +5,28 @@ class Verdict {
     this.id = id;
     this.type = type;
   }
-
+  // Regresa todos los elementos que cumplen con las restricciones establecidas
   static async getAll() {
     const data = processResult(await db.selectAll('Verdict','','','id',true,10));
     return data;
   }
-
+  // Regresa un solo elemento que cumple con las restricciones establecidas
   static async get(verdictId) {
     const data = await db.selectOne('Verdict', '',[{attr:'id',oper:'=',val:verdictId}]);
     return data.length !== 0 ? new Verdict(data[0]) : data;
   }
-
-  static async create({ type }) {
-    let response = await db.insert('Verdict', { type });
-    const id = response.insertId;
-    if (id > 0) {
-      return new Verdict({ id, type });
-    }
-    return [];
+  // Actualiza el elemento en la tabla si este ya existe, sino lo crea
+  static async save(){
+    if (this.id !== undefined && this.processResult(await db.get('Verdict', 'id', [{ attr: 'id', oper: '=', val: this.id }])).length !== 0) return this.update();
+    if (await db.create('Verdicts', this)) return 0;
+    return 1;
   }
-
+  // Actualiza el elemento
   static async update() {
     if (this.id !== undefined && await db.update('Verdict', this, [{ attr: 'id', oper: '=', val: this.id }])) return 0;
     return 1;
   }
-
+  // Elimina el elemento en la tabla por indice
   static async delete() {
     if (this.id !== undefined && processResult(await db.get('Verdict', 'id', [{ attr: 'id', oper: '=', val: this.id }])).length !== 0) {
       if (this.id !== undefined && await db.delete('Verdict', [{ attr: 'id', oper: '=', val: this.id }]) !== undefined) return 0;
@@ -48,3 +45,13 @@ function processResult(data) {
 }
 
 module.exports = Verdict;
+
+
+// static async create() {
+//   let response = await db.insert('Verdict', this);
+//   const id = response.insertId;
+//   if (id > 0) {
+//     return new Verdict({ id, type });
+//   }
+//   return [];
+// }
