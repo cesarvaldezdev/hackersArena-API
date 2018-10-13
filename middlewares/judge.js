3cb1e633c9d78ad3fc2708120ada8a8f62a62a9a
MatchaFierce <cesarvaldez.work@gmail.com>
2018-10-13 09:56:18 -0500

    Added judge

---
+++ middlewares/judge.js
@@ -0,0 +1,18 @@
+const hackerEarth = require('hackerearth-node');
+
+const hackerEarthConnection = new hackerEarth(
+  process.ENV.CLIENT_SECRET_KEY,
+  '0'
+)
+
+class Judge {
+  constructor(config = {}) {
+    this.config = config;
+  }
+
+}
+var config = {};
+config.time_limit = 1;
+config.memory =
+
+modules.exports = Judge;
