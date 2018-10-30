615f1028d3ce6ec178d538cbd77c94c82fc331d0
francoarielramirezvilla <franco.ramirez@alumno.udg.mx>
2018-10-30 01:15:25 -0600

    Merge branch 'Auth' of github.com:MatchaFierce/hackersArena-API into Auth

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,31 +3,60 @@ const { TokenCtrl } = require('../controllers');
 const { User, Token } = require('../models');
 
 const saltRounds = 10;
-
+/**
+ * Class for all the Authentication methods :
+ * - login
+ * - logout
+ * - Registrer
+ *
+ * This class also creates the tokens if needed.
+ */
 class Auth {
+  /**
+   * This metod confirms a registration process.
+   * If the token is still active, the user registration process continue.
+   * @param  {object}   req body of the request
+   * @param  {object}   res body of the response
+   * @param  {Function} next next funtion to execute next
+   * @return {Promise}       returns a created user
+   */
   static async register(req, res, next) {
-    const user = User.create(req);
-    // Crear el token
-    const hash = bcrypt.hashSync(`${user.name}${new Date()}`, saltRounds);
-    TokenCtrl.create({
-      token: hash,
-      createdAt: new Date(),
-      duration: 12,
-      type: 'Login',
-      active: 1,
-      aliasUser: user.aliasUser,
-    });
-    res.send({
-      data: {
-        hash,
-      },
-    }).status(201); // Sucesfully created
-    next();
+    const data = await TokenCtrl.get(req.params.emailToken);
+    if (data !== 0) { // Confirmation token exists
+      const user = User.create(req);
+      // Crear el token con el nombre del usuario+la fecha actual
+      const hash = bcrypt.hashSync(`${user.name}${new Date()}`, saltRounds);
+      TokenCtrl.create({
+        token: hash,
+        createdAt: new Date(),
+        duration: 12,
+        type: 'FirstLogin',
+        active: 1,
+        aliasUser: user.aliasUser,
+      });
+      res.send({
+        data: {
+          hash,
+        },
+      }).status(201).send({ message: 'User created!' }); // Sucessfully created
+      next();
+    } else {
+      // Token not match
+      res.status(401).send({ error: 'Invalid or inactive token' });
+    }
   }
 
+  /**
+   * This metod allows to login to the page if the user and password match,
+   * then creates a token for the started session.
+   * @param  {object}   req body of the request
+   * @param  {object}   res body of the response
+   * @param  {Function} next next funtion to execute next
+   * @return {Promise}       returns a the started session
+   */
   static async login(req, res, next) {
     const user = User.get(req.params.userAlias);
-    const token = Token.get(user.userAlias);
+    const token = Token.get(req.params.userAlias);
     if (token.status === 1) {
       // go to home page, alredy logged
     }
@@ -46,8 +75,16 @@ class Auth {
     }
   }
 
+  /**
+   * This metod ends the active session of a user by changing the
+   * token status to 0.
+   * @param  {object}   req body of the request
+   * @param  {object}   res body of the response
+   * @param  {Function} next next funtion to execute next
+   * @return {Promise}       returns the ended session
+   */
   static async logout(req, res, next) {
-    const token = Token.get(req.aliasUser);
+    const token = Token.get(req.params.aliasUser);
     if (token.status === 1) {
       // Change status to 0
     } else {
@@ -57,12 +94,21 @@ class Auth {
     next();
   }
 
-  static async session(req, next) {
+  /**
+   * This metod verify if the session is still active,
+   * if it is, allows to continue in the page.
+   * @param  {object}   req body of the request
+   * @param  {object}   res body of the response
+   * @param  {Function} next next funtion to execute next
+   * @return {Promise}       returns a created user
+   */
+  static async session(req, res, next) {
     const token = Token.get(req.aliasUser);
     if (token.status === 1) {
       next();
     } else {
       // Status is inactive
+      res.status(401).send({ error: 'Inactive token. ' });
     }
   }
 }
